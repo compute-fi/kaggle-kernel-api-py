85c5327c6ca5b9286d3b83998347fb2d778449f5
madhukar123456 <shroudmg@gmail.com>
2023-11-21 09:23:20 +0545

    first commit

--- src/utils/commands/status.py
+++ src/utils/commands/status.py
@@ -1,4 +1,4 @@
-from ...run_terminal import run_terminal_commands  # Assuming run_terminal_commands is defined in run_terminal.py
+import subprocess
 
-async def run_status_commands(id):
-    await run_terminal_commands(['kaggle', 'kernels', 'status', str(id)])
+async def run_status_commands(targetFolder):
+    subprocess.run(['kaggle', 'kernels', 'status', str(id)])
